8e3792489115d47a3d6727fccbce71a4e2c41355
Rachele Edwards <dot.com1472@gmail.com>
2020-07-17 09:29:55 -0600

    finished ring buffer --passing MVP will come back for last test if I have time

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,12 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
+        self.capacity = capacity
         self.size = 0
         self.storage = []
-        self.capacity = capacity
         self.oldest = len(self.storage)-len(self.storage)
 
     def append(self, item):
-        pass
+        if self.size == self.capacity:
+            print('IF')
+            #need to pop the oldest index
+            self.storage.pop(self.oldest)
+            self.storage.insert(self.oldest, item)
+            self.oldest += 1
+            # self.storage.append(item)
+        #otherwise
+        else:
+            #we add it to the end
+            self.storage.append(item)
+            self.size += 1
+            print(f'ELSE--adding size: {self.size}')
 
     def get(self):
-        pass
+        return self.storage
