485944ae26d3a6a2d3979ccd7d4ff1d31fdcbc38
Rachele Edwards <dot.com1472@gmail.com>
2020-07-17 09:53:53 -0600

    working and optimizes time but only showing Julio Rose--debugging

--- names/names.py
+++ names/names.py
@@ -13,10 +13,53 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    def __str__(self):
+        print(f'{self.value}')
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+        elif target > self.value:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+
+bst = BSTNode(names_1[0])
+
+for name1 in names_1:
+    bst.insert(name1)
+
+for name2 in names_2:
+    if bst.contains(name2):
+        duplicates.append(name1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
